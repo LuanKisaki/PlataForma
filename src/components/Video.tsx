4c09355c570de22d8d50db8f9bf9cd4e2d45004b
LKisaki <95627229+LKisaki@users.noreply.github.com>
2022-06-22 20:57:27 -0300

    criação de componentes

---
+++ src/components/Video.tsx
@@ -0,0 +1,24 @@
+export function Video() {
+    return (
+        <>
+        <div className="flex-1">
+            <div>
+                <div></div>
+            </div>
+        </div>
+
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+
+}
