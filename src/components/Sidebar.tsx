af6e9fd2c182fd67efea6c0ea36a3aa3a5825e47
LKisaki <95627229+LKisaki@users.noreply.github.com>
2022-06-23 03:16:50 -0300

    alterando escala de cor

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -28,8 +28,8 @@ export function Sidebar() {
     console.log(data)
 
     return (
-        <aside className="w-1/3 bg-gray-888 p-6 border-l border-myblue-999">
-            <span className="font-bold text-2xl pb-6 mb-6 border-b border-myblue-999 block">
+        <aside className="w-1/3 bg-gray-888 p-6 border-l border-myblue-888">
+            <span className="font-bold text-2xl pb-6 mb-6 border-b border-myblue-888 block">
                 Sidebar
             </span>
 
