4ea4df6a322f17f2ec42a551a7378965087ab882
LKisaki <95627229+LKisaki@users.noreply.github.com>
2022-06-23 02:26:47 -0300

    configurando data

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,25 +1,52 @@
-import { } from 'phosphor-react'
+import { CheckCircle, Lock } from 'phosphor-react'
+import { isPast, format} from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
-export function Lesson() {
+
+interface LessonProps {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonProps) {
+  const isLessonAvailable = isPast(props.availableAt)
+  const availableDateFormatted = format (props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+    locale: ptBR,
+  })
+  
     return (
         <a href="#">
             <span className="text-gray-500">
-                Segunda
+                { availableDateFormatted }
             </span>
 
             <div className="rounded border border-blue-999 mt-2 p-4 ">
                 
                 <header className="flex items-center justify-between">
-                    <span className="text-sm text-blue-666 font-medium">
-                        Status
+                  
+                  {
+                  isLessonAvailable ? (
+  
+                    <span className="text-sm text-blue-666 font-medium flex items-center gap-2">
+                    <CheckCircle size={20}/>
+                      Liberado
                     </span>
+                  ) : (
+                    <span className="text-sm text-orange-999 font-medium flex items-center gap-2">
+                    <Lock size={20} />
+                      Em breve
+                    </span>
+                  )}
+                
                     <span className="text-xs rounded px-2 py-[0.125rem] text-white  border border-green-300 font-bold ">
-                        Ao vivo
+                        { props.type === 'live' ? 'AO VIVO' : 'VIDEO' }
                     </span>
                 </header>
                 
                 <span className="font-bold text-gray-300 mt-5 block">
-                    Video 01
+                    { props.title }
                 </span>
             </div> 
         </a>
