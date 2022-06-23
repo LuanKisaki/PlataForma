3dc437b62cf3e7926a27164992ef50f74438a76e
LKisaki <95627229+LKisaki@users.noreply.github.com>
2022-06-23 02:25:47 -0300

    consumindo api

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,51 @@
-import { Lesson } from "./Lesson";
+import { gql, useQuery } from '@apollo/client'
+import { Lesson } from "./Lesson"
+
+const GET_LESSONS_QUERY = gql`
+    query MyQuery {
+  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+    id
+    lessonType
+    availableAt
+    title
+    slug
+  }
+}
+`
+interface GetLessonsQueryResponse {
+    lessons: {
+        id: string
+        title: string
+        slug: string
+        availableAt: string
+        lessonType: 'live' | 'class'
+    }[]
+}
 
 export function Sidebar() {
+    const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+
+    console.log(data)
+
     return (
         <aside className="w-1/3 bg-gray-888 p-6 border-l border-myblue-999">
             <span className="font-bold text-2xl pb-6 mb-6 border-b border-myblue-999 block">
                 Sidebar
             </span>
+
             <div className="flex flex-col gap-8">
-                <Lesson />
-                <Lesson />
-                <Lesson />
-                <Lesson />
-                <Lesson />
-                <Lesson />
+                {data?.lessons.map(lesson => {
+                    return (
+                        <Lesson
+                            key={lesson.id}
+                            title= {lesson.title}
+                            slug= {lesson.slug}
+                            availableAt={new Date(lesson.availableAt)}
+                            type= {lesson.lessonType}                 
+                        />
+                    )
+                })}
             </div>
         </aside>
     )
-
 }
