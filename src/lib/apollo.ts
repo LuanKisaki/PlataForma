bab6c3f688dfe4743c05c79eba3ff253ff5941ac
LKisaki <95627229+LKisaki@users.noreply.github.com>
2022-06-25 18:31:44 -0300

    importando caminho da API

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ow0max1aot01z402b3fh4x/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
