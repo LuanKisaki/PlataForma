95140923cffa0b4876e6215155e5b0a592c9b295
LKisaki <95627229+LKisaki@users.noreply.github.com>
2022-07-03 22:12:52 -0300

    responsive-layout tela de cadastro

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,7 @@
 export function Logo() {
   return (
     <svg
+      className=" m-auto sm:m-0"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       width="194.567"
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -30,9 +30,11 @@ export function Subscribe() {
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
       <div className="bg-react bg-top bg-no-repeat">
-        <div className=" w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-          <div className="max-w-[640px] ">
-            <Logo />
+        <div className="container w-full max-w-6xl grid gap-4 justify-between mt-20 mx-auto sm:grid-cols-2">
+          <div className="max-w-2xl pb-4 text-center sm:text-left">
+            <div className="m-auto">
+              <Logo />
+            </div>
 
             <h1 className="mt-8 text-[2.5rem] leading-tight">
               Construa uma aplicação <span className="font-bold text-blue-300">Completa</span>, do zero, com <span className="font-bold text-blue-300">React</span>
@@ -43,40 +45,41 @@ export function Subscribe() {
 
           </div>
 
-          <div className="p-9 border border-blue-999 rounded">
-            <span className="font-bold"> Inscreva-se</span>
+          <div className="max-w-lg sm:justify-self-end">
+            <div className="p-9 max-w-xs mx-auto border border-blue-999 rounded">
+              <span className="font-bold"> Inscreva-se</span>
 
-            <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
-              <input
-                type="text"
-                required
-                className="bg-gray-900 rounded px-5 h-14"
-                placeholder="Seu nome completo"
-                onChange={event => setName(event.target.value)}
-              />
-              <input 
-                type="email"
-                required
-                className="bg-gray-900 rounded px-5 h-14"
-                placeholder="Seu e-mail"
-                onChange={event => setEmail(event.target.value)}
-              />
-              <button
-                type="submit"
-                disabled={loading}
-                className="mt-4 bg-blue-777 uppercase py-4 rounded font-bold text-sm hover:bg-blue-888 hover:text-gray-400 transition-colors disabled:opacity-50"
-                >
-                Cadastrar
-              </button>
-
-            </form>
-
+              <form onSubmit={handleSubscribe} className="flex flex-col gap-2 mt-4 w-full">
+                <input
+                  type="text"
+                  required
+                  className="bg-gray-900 rounded px-5 h-14"
+                  placeholder="Seu nome completo"
+                  onChange={event => setName(event.target.value)}
+                />
+                <input 
+                  type="email"
+                  required
+                  className="bg-gray-900 rounded px-5 h-14"
+                  placeholder="Seu e-mail"
+                  onChange={event => setEmail(event.target.value)}
+                />
+                <button
+                  type="submit"
+                  disabled={loading}
+                  className="mt-4 bg-blue-777 uppercase py-4 rounded font-bold text-sm hover:bg-blue-888 hover:text-gray-400 transition-colors disabled:opacity-50"
+                  >
+                  Cadastrar
+                </button>
+              </form>
+            </div>
           </div>
-
         </div>
-        {/* <ReactSimbol /> */}
 
-        <img src="/src/assets/home_image_01.png" className="mt-10" alt=""/>
+        <div className=" w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+          <img src="/src/assets/home_image_01.png" className="mt-10" alt=""/>
+        </div>
+
       </div>
     </div>
   )
